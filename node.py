98d3fe40e3f0661c7f55bc45dc0b7bae0a87ce36
Ali Zaringhalam <azaringhalam@juniper.net>
2016-07-08 15:08:45 -0400

    remove password

--- node.py
+++ node.py
@@ -53,7 +53,7 @@ routers = [
 
 url = "https://10.10.2.25:8443/oauth2/token"
 
-payload = {'grant_type': 'password', 'username': 'some_name', 'some_password': 'northstar123'}
+payload = {'grant_type': 'password', 'username': 'some_name', 'password': 'some_password'}
 response = requests.post (url, data=payload, auth=('some_name','some_password'), verify=False)
 json_data = json.loads(response.text)
 authHeader= {"Authorization":"{token_type} {access_token}".format(**json_data)}
